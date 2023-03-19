575aa3b8abb2ad56ae4c1371cdfbf472b904ce4b
Davi Luna <86129094+davifurao@users.noreply.github.com>
2023-03-18 23:55:59 -0300

    aplicando as opções sim no instalador de pendências

--- IOT/Instalador-pendencias.sh
+++ IOT/Instalador-pendencias.sh
@@ -3,11 +3,11 @@ saida_instrucao = "Caso você queira terminar a operação, aperte Ctrl+c"
 function atualizacao(){
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt install mosquitto
-sudo apt install mosquitto-clients
-sudo apt-get install python3
-sudo apt-get install python3-pip
-pip install paho-mqtt
+sudo apt install mosquitto -y
+sudo apt install mosquitto-clients -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+pip install paho-mqtt -y
 sudo apt-get update
 sudo apt-get upgrade
 sudo service mosquitto start
