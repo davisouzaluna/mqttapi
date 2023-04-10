32fdf204480f43c2c943cc3d7a26b1048d833a67
Davi Luna <Sdavi738@gmail.com>
2023-04-09 22:30:36 -0300

    Update the API, with database connection

--- IOT/Instalador-pendencias.sh
+++ IOT/Instalador-pendencias.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+#instalador de pendências para a utilização correta 
+#do programa
+
+#Precisamos do: Mosquitto, mosquitto-clients, pip(para a partir dele instalarmos o paho-mqtt)
+#E após tudo isso iniciarmos o broker.
+
+
 # Verifica se o pacote pip está instalado
-if ! command -v pip &> /dev/null
+if ! command -v pip &> /dev/null # Esse comando descarta a saída padrão(true). Ou seja, caso seja false, não irá ser imprimido nada na tela, já que a saída padrão é a função do dado comando
 then
     echo "pip não está instalado. Instalando..."
     sudo apt-get update
-    sudo apt-get install python-pip -y
+    sudo apt-get install python3-pip -y
 fi
 
 # Verifica se o pacote paho-mqtt está instalado
-if ! python -c "import paho.mqtt.client" &> /dev/null
+if ! python3 -c "import paho.mqtt.client" &> /dev/null # Esse comando descarta a saída padrão(true). Ou seja, caso seja false, não irá ser imprimido nada na tela, já que a saída padrão é a função do dado comando
 then
     echo "paho-mqtt não está instalado. Instalando..."
     sudo pip install paho-mqtt
 fi
 
 # Verifica se o pacote mosquitto está instalado
-if ! command -v mosquitto &> /dev/null
+if ! command -v mosquitto &> /dev/null # Esse comando descarta a saída padrão(true). Ou seja, caso seja false, não irá ser imprimido nada na tela, já que a saída padrão é a função do dado comando
 then
     echo "mosquitto não está instalado. Instalando..."
     sudo apt-get update
@@ -24,20 +31,18 @@ then
 fi
 
 # Verifica se o pacote mosquitto-clients está instalado
-if ! command -v mosquitto_sub &> /dev/null
+if ! command -v mosquitto_sub &> /dev/null # Esse comando descarta a saída padrão(true). Ou seja, caso seja false, não irá ser imprimido nada na tela, já que a saída padrão é a função do dado comando
 then
     echo "mosquitto-clients não está instalado. Instalando..."
     sudo apt-get update
     sudo apt-get install mosquitto-clients -y
 fi
-#Verifica se o socket io está instalado
+#verifica se o pymysql tá instalado
+if ! dpkg -s python3-pymysql >/dev/null 2>&1; then
+    echo "O pacote pymysql não está instalado. Instalando..."
+    sudo apt-get update
+    pip install pymysql -y # instala o pymysql
 
-if [ -d "./node_modules/socket.io" ]
-then
-  echo "O pacote Socket.io está instalado."
-else
-  npm install socket.io
 fi
-
 # Inicia o broker Mosquitto
 sudo service mosquitto start
